5586d25c552e681b43018e0d4a909fdc2333b88f
unknown <tranthuongpro99@gmail.com>
2021-11-29 16:50:56 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { KeycodeInput } from './components/KeyCode';
 
-export default function App() {
+const App = () => {
+  const [value, setValue] = useState('');
+  const [numeric, setNumeric] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.text}>Enter your access code</Text>
+
+      <KeycodeInput
+        numeric={numeric}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+        onComplete={(completedValue) => {
+          alert('Completed! Value: ' + completedValue);
+        }}/>
+
+      <View style={styles.button}>
+        <Button
+          color='#9b9b9b'
+          title='Set value'
+          onPress={() => setValue('33')}/>
+        <Button
+          color='#9b9b9b'
+          title='Reset value'
+          onPress={() => setValue('')}/>
+        <Button
+          color='#9b9b9b'
+          title={!numeric ? 'Switch to numeric mode' : 'Switch to alpha mode'}
+          onPress={() => {
+            setValue('');
+            setNumeric(!numeric);
+          }}/>
+      </View>
+
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingBottom: 200
   },
+  text: {
+    fontSize: 18,
+    marginBottom: 32
+  },
+  button: {
+    marginTop: 96
+  }
 });
